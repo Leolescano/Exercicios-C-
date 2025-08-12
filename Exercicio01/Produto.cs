deba47d7d1ed2a94ac980260f3d18ed0e299cc6e
Leonardo Lescano <leolescanomdq@gmail.com>
2025-08-12 14:42:56 -0300

    Resolvido o Exercio01

---
+++ Exercicio01/Produto.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio01;
+
+public class Produto
+{
+    private static int contadorId = 0; 
+
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public decimal Preco { get; private set; }
+
+    
+    private Produto(string nome, decimal preco)
+    {
+        Id = ++contadorId;
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public static Produto Criar(string nome, decimal preco)
+    {
+        return new Produto(nome, preco);
+    }
+
+    public override string ToString()
+    {
+        return $"Produto: {Id} - Nome: {Nome} - Preço: R${Preco:N2}";
+    }
+}
---
+++ Exercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿using Exercicio01;  
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var produto1 = Produto.Criar("Mouse Gamer", 150.00m);
+        var produto2 = Produto.Criar("Teclado Mecânico", 350.00m);
+
+        Console.WriteLine(produto1);
+        Console.WriteLine(produto2);
+    }
+}
